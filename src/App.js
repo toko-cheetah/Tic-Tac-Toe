58e5d8c90d927c58e0639dc3797774711ec1d394
Tornike Khatiashvili <100285138+toko-cheetah@users.noreply.github.com>
2022-08-12 15:13:08 +0400

    Set winner added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NewGame from "./Components/NewGame";
 import Game from "./Components/Game";
@@ -14,6 +14,7 @@ export default function App() {
 
   const [xTurn, setXTurn] = useState(true);
   const [grid, setGrid] = useState(defaultGrid);
+  const [winner, setWinner] = useState("");
   const [score, setScore] = useState({
     x: 0,
     ties: 0,
@@ -29,13 +30,32 @@ export default function App() {
     markO: player1sMarkX ? players.player2 : players.player1,
   };
 
-  // console.log(grid);
-
   const getEl = (id) => document.getElementById(id);
+
+  console.log(endGame, winner);
+
+  useEffect(() => {
+    findOutWinner("space1", "space2", "space3");
+    findOutWinner("space4", "space5", "space6");
+    findOutWinner("space7", "space8", "space9");
+    findOutWinner("space1", "space4", "space7");
+    findOutWinner("space2", "space5", "space8");
+    findOutWinner("space3", "space6", "space9");
+    findOutWinner("space1", "space5", "space9");
+    findOutWinner("space3", "space5", "space7");
+  }, [xTurn, grid]);
+
+  function findOutWinner(spaceNum1, spaceNum2, spaceNum3) {
+    return (
+      grid[spaceNum1].filled &&
+      grid[spaceNum1].mark === grid[spaceNum2].mark &&
+      grid[spaceNum1].mark === grid[spaceNum3].mark &&
+      (setWinner(grid[spaceNum1].mark), setEndGame(true))
+    );
+  }
 
   function selectMark(event) {
     const e = event.target;
-
     return e.id === "select-x"
       ? (setPlayer1sMarkX(true),
         e.classList.add("x-selected"),
@@ -47,7 +67,6 @@ export default function App() {
 
   function selectRival(event) {
     const e = event.target;
-
     return (
       e.id === "vs-cpu" ? setVsCpu(true) : setVsCpu(false), setNewGame(false)
     );
@@ -55,7 +74,6 @@ export default function App() {
 
   function selectSpace(event) {
     const e = event.target;
-
     return (
       !grid[e.id].filled &&
       (setGrid((prev) => ({
